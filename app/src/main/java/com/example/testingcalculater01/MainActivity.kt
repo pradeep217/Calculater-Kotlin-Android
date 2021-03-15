2c4017bb69b0a72a2fd2373e2331691567d6bad1
DELL <pradeepkumar.k@fimo.co.in>
2021-03-15 23:03:17 +0530

    Code Working

--- app/src/main/java/com/example/testingcalculater01/MainActivity.kt
+++ app/src/main/java/com/example/testingcalculater01/MainActivity.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import net.objecthunter.exp4j.Expression
 import net.objecthunter.exp4j.ExpressionBuilder
 import java.lang.Exception
 
+//
 private lateinit var resultTextView: TextView
 private lateinit var operationTextView: TextView
 
@@ -23,99 +25,75 @@ class MainActivity : AppCompatActivity() {
         //textView
         resultTextView = findViewById(R.id.textView1)
         operationTextView = findViewById(R.id.textView2)
-//        signTextView = findViewById(R.id.textView3)
-        //numbers
-        var oneButton: Button = findViewById(R.id.button1)
-        var twoButton: Button = findViewById(R.id.button2)
-        var threeButton: Button = findViewById(R.id.button3)
-        var fourButton: Button = findViewById(R.id.button4)
-        var fiveButton: Button = findViewById(R.id.button5)
-        var sixButton: Button = findViewById(R.id.button6)
-        var sevenButton: Button = findViewById(R.id.button7)
-        var eightButton: Button = findViewById(R.id.button8)
-        var nineButton: Button = findViewById(R.id.button9)
-        var zeroButton: Button = findViewById(R.id.buttonZero)
-
-        //operation Button
-        var clearButton: Button = findViewById(R.id.buttonClearText)
-        var bracketButton: Button = findViewById(R.id.buttonBracket)
-        var persentageButton: Button = findViewById(R.id.buttonPercentage)
-        var divideButton: Button = findViewById(R.id.buttonDivide)
-        var addButton: TextView = findViewById(R.id.buttonAdd)
-        var subtractionButton: Button = findViewById(R.id.buttonSubtraction)
-        var multiplyButton: Button = findViewById(R.id.buttonMultiply)
-        var equalButton: Button = findViewById(R.id.buttonEqual)
-        var pointButton: Button = findViewById(R.id.buttonPoint)
-        var backButton: Button = findViewById(R.id.buttonBack)
 
 
         //numbers
-        oneButton.setOnClickListener {
+        button1.setOnClickListener {
             appendonExpresstion("1", true)
         }
-        twoButton.setOnClickListener {
+        button2.setOnClickListener {
             appendonExpresstion("2", true)
         }
-        threeButton.setOnClickListener {
+        button3.setOnClickListener {
             appendonExpresstion("3", true)
         }
-        fourButton.setOnClickListener {
+        button4.setOnClickListener {
             appendonExpresstion("4", true)
         }
-        fiveButton.setOnClickListener {
+        button5.setOnClickListener {
             appendonExpresstion("5", true)
         }
-        sixButton.setOnClickListener {
+        button6.setOnClickListener {
             appendonExpresstion("6", true)
         }
-        sevenButton.setOnClickListener {
+        button7.setOnClickListener {
             appendonExpresstion("7", true)
         }
-        eightButton.setOnClickListener {
+        button8.setOnClickListener {
             appendonExpresstion("8", true)
         }
-        nineButton.setOnClickListener {
+        button9.setOnClickListener {
             appendonExpresstion("9", true)
         }
-        zeroButton.setOnClickListener {
+        buttonZero.setOnClickListener {
             appendonExpresstion("0", true)
         }
-        pointButton.setOnClickListener {
+        buttonPoint.setOnClickListener {
             appendonExpresstion(".", true)
         }
         //operation
-        persentageButton.setOnClickListener {
+        buttonPercentage.setOnClickListener {
             appendonExpresstion("%", false)
         }
-        divideButton.setOnClickListener {
+        buttonDivide.setOnClickListener {
             appendonExpresstion("/", false)
         }
-        addButton.setOnClickListener {
-            Log.d("add button","working")
+        buttonAdd.setOnClickListener {
+            Log.d("add button", "working")
             appendonExpresstion("+", true)
         }
-        subtractionButton.setOnClickListener {
+        buttonSubtraction.setOnClickListener {
             appendonExpresstion("-", false)
         }
-        multiplyButton.setOnClickListener {
+        buttonMultiply.setOnClickListener {
             appendonExpresstion("*", false)
         }
-        equalButton.setOnClickListener {
+        buttonEqual.setOnClickListener {
             appendonExpresstion("=", false)
         }
 
-        clearButton.setOnClickListener {
+        buttonClearText.setOnClickListener {
             resultTextView.text = ""
             operationTextView.text = ""
         }
-        backButton.setOnClickListener {
+        buttonBack.setOnClickListener {
             var string = operationTextView.text.toString()
             if (string.isNotEmpty()) {
                 operationTextView.text = string.substring(0, string.length - 1)
             }
             resultTextView.text = ""
         }
-        equalButton.setOnClickListener {
+        buttonEqual.setOnClickListener {
             try {
                 val exception = ExpressionBuilder(operationTextView.text.toString()).build()
                 val result = exception.evaluate()
