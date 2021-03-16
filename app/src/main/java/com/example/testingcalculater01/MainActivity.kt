f13e177902a5e48ada6cebcc884b62e3d322feb5
DELL <pradeepkumar.k@fimo.co.in>
2021-03-16 22:05:56 +0530

    new Code

--- app/src/main/java/com/example/testingcalculater01/MainActivity.kt
+++ app/src/main/java/com/example/testingcalculater01/MainActivity.kt
@@ -1,85 +1,76 @@
 package com.example.testingcalculater01
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import net.objecthunter.exp4j.Expression
+import kotlinx.android.synthetic.main.activity_main.*//widget refrence in kotlin
 import net.objecthunter.exp4j.ExpressionBuilder
 import java.lang.Exception
 
-//
-private lateinit var resultTextView: TextView
-private lateinit var operationTextView: TextView
-
+private const val TAG = "MainActivity"
+private const val Result_Key = "TEXT_RESULT"
+private const val CONTENTS_Key = "TEXT_CONTENTS"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //textView
-        resultTextView = findViewById(R.id.textView1)
-        operationTextView = findViewById(R.id.textView2)
-
 
         //numbers
         button1.setOnClickListener {
-            appendonExpresstion("1", true)
+            appendExpression("1", true)
         }
         button2.setOnClickListener {
-            appendonExpresstion("2", true)
+            appendExpression("2", true)
         }
         button3.setOnClickListener {
-            appendonExpresstion("3", true)
+            appendExpression("3", true)
         }
         button4.setOnClickListener {
-            appendonExpresstion("4", true)
+            appendExpression("4", true)
         }
         button5.setOnClickListener {
-            appendonExpresstion("5", true)
+            appendExpression("5", true)
         }
         button6.setOnClickListener {
-            appendonExpresstion("6", true)
+            appendExpression("6", true)
         }
         button7.setOnClickListener {
-            appendonExpresstion("7", true)
+            appendExpression("7", true)
         }
         button8.setOnClickListener {
-            appendonExpresstion("8", true)
+            appendExpression("8", true)
         }
         button9.setOnClickListener {
-            appendonExpresstion("9", true)
+            appendExpression("9", true)
         }
         buttonZero.setOnClickListener {
-            appendonExpresstion("0", true)
+            appendExpression("0", true)
         }
         buttonPoint.setOnClickListener {
-            appendonExpresstion(".", true)
+            appendExpression(".", true)
         }
         //operation
         buttonPercentage.setOnClickListener {
-            appendonExpresstion("%", false)
+            appendExpression("%", false)
         }
         buttonDivide.setOnClickListener {
-            appendonExpresstion("/", false)
+            appendExpression("/", false)
         }
         buttonAdd.setOnClickListener {
             Log.d("add button", "working")
-            appendonExpresstion("+", true)
+            appendExpression("+", true)
         }
         buttonSubtraction.setOnClickListener {
-            appendonExpresstion("-", false)
+            appendExpression("-", false)
         }
         buttonMultiply.setOnClickListener {
-            appendonExpresstion("*", false)
+            appendExpression("*", false)
         }
         buttonEqual.setOnClickListener {
-            appendonExpresstion("=", false)
+            appendExpression("=", false)
         }
 
         buttonClearText.setOnClickListener {
@@ -87,7 +78,7 @@ class MainActivity : AppCompatActivity() {
             operationTextView.text = ""
         }
         buttonBack.setOnClickListener {
-            var string = operationTextView.text.toString()
+            val string = operationTextView.text.toString()
             if (string.isNotEmpty()) {
                 operationTextView.text = string.substring(0, string.length - 1)
             }
@@ -108,7 +99,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun appendonExpresstion(string: String, canClear: Boolean) {
+    private fun appendExpression(string: String, canClear: Boolean) {
 
         if (resultTextView.text.isNotEmpty()) {
             operationTextView.text = ""
@@ -121,5 +112,19 @@ class MainActivity : AppCompatActivity() {
             operationTextView.append(string)
             resultTextView.text = ""
         }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "Data Restoring successfully")
+        super.onRestoreInstanceState(savedInstanceState)
+        operationTextView.text = savedInstanceState.getString(CONTENTS_Key, "")
+        resultTextView.text = savedInstanceState.getString(Result_Key, "")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG, "data in saveInstanceState")
+        super.onSaveInstanceState(outState)
+        outState.putString(CONTENTS_Key, operationTextView.text.toString())
+        outState.putString(Result_Key, resultTextView.text.toString())
     }
 }
